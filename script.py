7062720bce3063d9dac6452341c3dcc7307fbe91
Muhammad Aun <ayekaunic@gmail.com>
2023-11-16 19:59:05 +0500

    Similarity using VGG16.

--- script.py
+++ script.py
@@ -1,10 +1,13 @@
 # imports
-import os
-import pandas as pd
 from PIL import Image
 import imagehash
-from skimage import io, metrics
+import os
+import pandas as pd
 from tqdm import tqdm
+import numpy as np 
+from tensorflow.keras.preprocessing import image
+from keras.applications.vgg16 import VGG16
+from sklearn.metrics.pairwise import cosine_similarity
 
 # functions
 def compute_hash(image_path):
@@ -13,10 +16,10 @@ def compute_hash(image_path):
     return hash_value
 
 def find_duplicate_images(images_directory):
-    image_files = [f for f in os.listdir(images_directory) if f.endswith(('.jpg', '.jpeg', '.png'))]
+    image_files = [f for f in os.listdir(images_directory) if f.endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp'))]
     num_files = len(image_files)
 
-    duplicates_matrix = pd.DataFrame(index=image_files, columns=image_files).fillna('_')
+    duplicates_matrix = pd.DataFrame(index=image_files, columns=image_files).fillna(' ')
 
     for i in tqdm(range(num_files), desc="Finding Duplicates"):
         for j in range(i + 1, num_files):
@@ -32,29 +35,57 @@ def find_duplicate_images(images_directory):
 
     duplicates_matrix.to_csv('duplicates.csv')
     
-def calculate_similarity(image1, image2):
-    if image1.shape != image2.shape:
-        return 0
-    return metrics.structural_similarity(image1, image2, channel_axis=2)
+def load_image(image_path):
+
+    input_image = Image.open(image_path)
+    resized_image = input_image.resize((224, 224))
+
+    return resized_image
+
+def get_image_embeddings(object_image : image):
+
+    image_array = np.expand_dims(image.img_to_array(object_image), axis = 0)
+    image_embedding = vgg16.predict(image_array, verbose=0)
+
+    return image_embedding
+
+def get_similarity_score(first_image_embedding, second_image_embedding):
+
+    similarity_score = cosine_similarity(first_image_embedding, second_image_embedding).reshape(1,)
+    return similarity_score
 
 def find_similar_images(images_directory):
     image_files = [f for f in os.listdir(images_directory) if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp'))]
 
     similarity_matrix = pd.DataFrame(index=image_files, columns=image_files)
+    image_embeddings = {}
 
-    for file1 in tqdm(image_files, desc="Calculating Similarty"):
+    for file in tqdm(image_files, desc="Calculating Embeddings"):
+        image_path = os.path.join(images_directory, file)
+        img = load_image(image_path)
+        img_embedding = get_image_embeddings(img)
+        image_embeddings[file] = img_embedding
+
+    for file1 in tqdm(image_files, desc="Calculating Similarity"):
         for file2 in image_files:
             if file1 == file2:
                 similarity = 0
             else:
-                image1 = io.imread(os.path.join(images_directory, file1))
-                image2 = io.imread(os.path.join(images_directory, file2))
-                similarity = calculate_similarity(image1, image2)
+                img_embedding1 = image_embeddings[file1]
+                img_embedding2 = image_embeddings[file2]
+                similarity = get_similarity_score(img_embedding1, img_embedding2)[0]
                 similarity = round(similarity, 3)
 
             similarity_matrix.at[file1, file2] = similarity
 
     similarity_matrix.to_csv("similarity.csv", index=True)
+
+# similarity model
+vgg16 = VGG16(weights='imagenet', include_top=False, 
+              pooling='max', input_shape=(224, 224, 3))
+
+for model_layer in vgg16.layers:
+  model_layer.trainable = False
     
 # generating reports
 if __name__ == '__main__':
